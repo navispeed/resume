ad302407932b3fc9efd152db018e9c6337f64d93
Grégoire GUEMAS <gregoire.guemas@epitech.eu>
2016-10-18 22:51:02 +0200

    adding yohann link for GA

--- routes/index.js
+++ routes/index.js
@@ -7,7 +7,7 @@ var fs = require("fs");
 router.get('/', function(req, res, next) {
   var content;
   content = fs.readFileSync("public/intro.txt", "UTF-8");
-  var link = "nothing";
+  var link = "";
   switch (req.headers.host) {
     case "navispeed.eu":
       link = "(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){ \
@@ -19,7 +19,12 @@ router.get('/', function(req, res, next) {
     ";
       break;
     case "yohanncelerien.com":
-      link = "";
+      link = "(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){  \
+      (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),        \
+      m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)       \
+      })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');    \
+      ga('create', 'UA-85942631-1', 'auto');                                                \
+            ga('send', 'pageview');";
       break;
   }
   res.render('index', {tab:content, analytics: link });
